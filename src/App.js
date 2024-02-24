c601ed19a19f79eb35da0aa5fae4d87f1b1da439
erincronie <erincronie@gmail.com>
2024-02-24 12:56:33 -0800

    created simple header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import '../src/styles/partials/_global.scss';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <div className='header__info'>
+                <h2 className='header__title'>
+                    Welcome!
+                </h2>
+                <h3 className='header__subtitle'>
+                    erincronie.me
+                </h3>
+            </div>
+            <nav className='header__nav'>
+                <ul className='header__nav-list'>
+                    <li className='header__nav-item'>Resume</li>
+                    <li className='header__nav-item'>Projects</li>
+                    <li className='header__nav-item'>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
