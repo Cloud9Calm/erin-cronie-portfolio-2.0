0bf253914584695dcd48b90793e97462e1af3669
erincronie <erincronie@gmail.com>
2024-02-24 20:31:26 -0800

    removed profile image and added name animations

---
+++ src/components/Name/Name.jsx
@@ -0,0 +1,15 @@
+import './Name.scss';
+
+const Name = () => {
+    return (
+        <div className="container">
+        <div className="row">
+          <div className="col-md-12 text-center">
+            <h3 className="animate-charcter"> ERIN CRONIE</h3>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Name;
