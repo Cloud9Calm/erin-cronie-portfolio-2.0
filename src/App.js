2412f0ab50fd24ee23f8736db9763a32fbc72d50
erincronie <erincronie@gmail.com>
2024-02-24 19:59:32 -0800

    added custom mouse

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import '../src/styles/partials/_global.scss';
+import AnimatedCursor from "react-animated-cursor";
 import Header from './components/Header/Header';
 import Profile from './components/Profile/Profile';
 
 function App() {
   return (
     <div className="App">
+      <AnimatedCursor />
       <Header />
       <Profile />
     </div>
