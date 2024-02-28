4ba828232a069081f41acbbd29493363e4118c37
erincronie <erincronie@gmail.com>
2024-02-27 21:12:56 -0800

    created social media section

--- src/components/SocialMedia/SocialMedia.jsx
+++ src/components/SocialMedia/SocialMedia.jsx
@@ -1,37 +1,59 @@
 import './SocialMedia.scss';
+import InstagramSVG from '../../assets/images/Instagram.svg';
+import LinkedInSVG from '../../assets/images/LinkedIn.svg';
+import GitHubSVG from '../../assets/images/GitHub.svg';
 
 const SocialMedia = () => {
     return (
         <section className='social-media'>
             <h2 className='social-media__title title'>Social Media</h2>
            <ul className='social-media__list'>
-           <li className='social-media__list-item'>
-                <a 
-                    href="https://github.com/erincronie" 
-                    target="_blank" 
-                    rel="noopener noreferrer">
-                        Find my GitHub
-                </a>
-            </li>
-            <li className='social-media__list-item'>
-                <a 
-                    href="https://www.instagram.com/cloud9calm" 
-                    target="_blank" 
-                    rel="noopener noreferrer">
-                        Follow me on Instagram
-                </a>
-            </li>
-            <li className='social-media__list-item'>
-                <a 
-                    href="https://www.linkedin.com/in/erin-cronie/" 
-                    target="_blank" 
-                    rel="noopener noreferrer">
-                        Connect with me on LinkedIn
-                </a>
-            </li>
+                <li className='social-media__list-item'>
+                    <a 
+                        className='social-media__link'
+                        href="https://github.com/erincronie" 
+                        target="_blank" 
+                        rel="noopener noreferrer">
+                            Find my GitHub
+                    </a>
+                    <img 
+                        src={GitHubSVG} 
+                        alt="GitHub Icon"
+                        className='social-media__svg' 
+                    />
+                </li>
+                <li className='social-media__list-item'>
+                    <a 
+                        className='social-media__link'
+                        href="https://www.instagram.com/cloud9calm" 
+                        target="_blank" 
+                        rel="noopener noreferrer">
+                            Follow me on Instagram
+                    </a>
+                    <img 
+                        src={InstagramSVG} 
+                        alt="Instagram Icon"
+                        className='social-media__svg' 
+                    />
+                </li>
+                <li className='social-media__list-item'>
+                    <a 
+                        className='social-media__link'
+                        href="https://www.linkedin.com/in/erin-cronie/" 
+                        target="_blank" 
+                        rel="noopener noreferrer">
+                            Connect with me on LinkedIn
+                    </a>
+                    <img 
+                        src={LinkedInSVG} 
+                        alt="LinkedIn Icon" 
+                        className='social-media__svg'
+                    />
+                </li>
            </ul>
         </section>
     );
 }
 
 export default SocialMedia;
+
