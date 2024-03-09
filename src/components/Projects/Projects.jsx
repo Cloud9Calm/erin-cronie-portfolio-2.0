93ce822bc4040f910cba5671ec69f2e24de4c24a
erincronie <erincronie@gmail.com>
2024-03-08 17:11:10 -0800

    removed server files

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,6 +6,9 @@ const Profile = () => {
         <section className='projects' id='projects'>
             <h2 className='projects__title title'>Projects</h2>
             <div className="projects__list">
+                <p className='projects__blurb'>
+                    I'm thrilled to share a sneak peek into some of the exciting projects I'm currently working on! While they're not yet available, I can't wait to unveil the innovative solutions and features they'll bring. Stay tuned for updates as I continue to refine and polish these projects to deliver the best possible experiences. Get ready to embark on an adventure filled with creativity, innovation, and endless possibilities!
+                </p>
                 {projectsData.projects.map((project, index) => (
                     <div key={index} className="projects__list-items">
                         <h3 className='projects__name'>{project.title}</h3>
