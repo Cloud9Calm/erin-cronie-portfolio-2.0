4ec389e9ebe3dcf7482a96261110a25b73b85975
erincronie <erincronie@gmail.com>
2025-03-05 20:09:49 -0800

    add more space to name section

--- src/components/Name/Name.jsx
+++ src/components/Name/Name.jsx
@@ -3,13 +3,11 @@ import './Name.scss';
 const Name = () => {
     return (
         <div className="container">
-        <div className="row">
-          <div className="col-md-12 text-center">
-            <h1 className="animate-charcter">ERIN CRONIE - Okanagan's Website & eCommerce Specialist</h1>
+            <h1 className="animate-character">
+              ERIN CRONIE - Okanagan's Website & eCommerce Specialist
+            </h1>
           </div>
-        </div>
-      </div>
     );
 }
 
-export default Name;
+export default Name
